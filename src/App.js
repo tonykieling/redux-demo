07b425e89d1fe88135f177b2db768ef852552189
Tony Kieling <tony.kieling@gmail.com>
2019-05-10 10:05:10 -0700

    doing Postform

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import './App.css';
 import Posts from './components/Posts.js'
+import Postform from './components/Postform.js'
 
 function App() {
   return (
     <div className="App">
+      <Postform />
+      <hr />
       <Posts />
     </div>
   );
--- src/components/Postform.js
+++ src/components/Postform.js
@@ -1,12 +1,26 @@
 import React, { Component } from 'react'
 
-export default class Postform extends Component {
+class Postform extends Component {
   render() {
     return (
       <div>
-        
+        <h1>Postform is here</h1>
+        <form>
+          <div>
+            <label>Name: </label>
+            <input type="text" />
+          </div>
+          <br />
+          <div>
+            <label>Email: </label>
+            <input type="text" />
+          </div>
+          <br />
+          <button type="submit">Save</button>
+        </form>
       </div>
     )
   }
 }
 
+export default Postform;
