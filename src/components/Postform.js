407ded0e9a8b601f592159da41bcf92ab5a60549
Tony Kieling <tony.kieling@gmail.com>
2019-05-10 10:29:56 -0700

    Postfrm almost done

--- src/components/Postform.js
+++ src/components/Postform.js
@@ -1,19 +1,52 @@
 import React, { Component } from 'react'
 
 class Postform extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: "",
+      email: ""
+    }
+  }
+
+  handleChange = e => {
+    this.setState({
+      [e.target.name]: e.target.value
+    })
+    console.log(e.target)
+  }
+
+  handleSubmit = e => {
+    e.preventDefault();
+    const post = {
+      name: this.state.name,
+      email: this.state.email
+    }
+console.log("post: ", post)
+    // fetch('https://jsonplaceholder.typicode.com/users', {
+    //   method: "POST",
+    //   headers: {
+    //     "content-type": "application/json"
+    //   },
+    //   body: JSON.stringify(post)
+    // })
+    // .then(res => res.json)
+    // .then(data => console.log(data))
+  }
+
   render() {
     return (
       <div>
         <h1>Postform is here</h1>
-        <form>
+        <form onSubmit={this.handleSubmit}>
           <div>
             <label>Name: </label>
-            <input type="text" />
+            <input type="text" value={this.state.name} onChange={this.handleChange} name="name"/>
           </div>
           <br />
           <div>
             <label>Email: </label>
-            <input type="text" />
+            <input type="text" value={this.state.email} onChange={this.handleChange} name="email"/>
           </div>
           <br />
           <button type="submit">Save</button>
