72abaaa50b0b32a85b12a0a72c52dd3f4f75d676
Tony Kieling <tony.kieling@gmail.com>
2019-05-10 09:57:53 -0700

    doing Posts pretty done

--- src/components/Posts.js
+++ src/components/Posts.js
@@ -2,42 +2,43 @@ import React, { Component } from 'react'
 
 class Posts extends Component {
   constructor(props) {
-    super(props)
+    super(props);
     this.state = {
-      id: "",
-      name: ""
-    }
+      posts: []
+    };
   }
 
   componentWillMount() {
     fetch('https://jsonplaceholder.typicode.com/users')
       .then(res => res.json())
-      .then(data => {
-        console.log(data)
-        data.forEach(element => {
-          console.log("element: ", element)
-          this.setState({
-            id: element.id,
-            name: element.name
-          })
-        });
-        // this.setState({
-        //   id: data.id,
-        //   name: data.name
-        // })
-      })
-      // .then(console.log("this.state: ", this.state))
+      .then(data => this.setState({posts: data}))
+        // data.forEach(element => {
+        //   console.log("element: ", element)
+        //   this.setState({
+        //     posts: [
+        //       ...this.state.posts, 
+        //       [{id: element.id,
+        //       name: element.name}]
+        //     ]
+        //   })
+        // });
   }
 
-  componentDidMount() {
-    console.log("DIDthis.state: ", this.state)
-  }
+  // componentDidMount() {
+  //   console.log("DIDthis.state: ", this.state)
+  // }
 
   render() {
+    const postItems = this.state.posts.map(post => (
+      <div key={post.id}>
+        <h3>id: {post.id}</h3>
+        <h3>name: {post.name}</h3>
+      </div>
+    ))
     return (
       <div>
         <h1>Posts component</h1>
-        
+        {postItems}
       </div>
     )
   }
