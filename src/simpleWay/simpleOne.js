bfb88516d79e00135ebb8c73535362fd8fc50094
Tony Kieling <tony.kieling@gmail.com>
2019-05-10 18:33:05 -0700

    store, reducer, dispatch and subscribe are workinggit add .

--- src/simpleWay/simpleOne.js
+++ src/simpleWay/simpleOne.js
@@ -1,6 +1,7 @@
 const { createStore } = require('redux');
 
 // create initial state
+// const initialState = 10;
 const initialState = {
   number: 10
 }
@@ -8,45 +9,36 @@ const initialState = {
 // create reducer
 const myReducer = (state = initialState, action) => {
   const newState = {...state};
-  console.log("newState: ", newState, " action: ", action);
-  switch (action.type) {
-    case "ADD":
-      return newState.number = newState.number + 1;
-      // console.log("ADD action");
-    case "SUB":
-      return newState.number = newState.number - 1;
-      // console.log("SUB action");
-    default:
-      return newState;
-      // console.log("NO action selected");
-  }
+  console.log("= ", newState);
+  if (action.type === "ADD")
+    newState.number = newState.number + 1;
+  if (action.type === "SUB")
+    newState.number = newState.number - 1;
+
+  return newState;
 }
 
 // create store
 console.log("create store");
 const store = createStore(myReducer);
 
-// // dispatching ADD
-// store.dispatch({type: "SUB"});
-// // checking state
-// console.log("Store is now = " + store.getState());
+// subscribe is gonna be executed every time a change happens
+store.subscribe(() => {
+  console.log("state changed: ", store.getState());
+})
 
-// dispatching ADD
-store.dispatch({type: "SUB"});
-// checking state
-console.log("Store is now = " + JSON.stringify(store.getState()));
+
+// // dispatching ADD
+store.dispatch({type: "ADD"});
 
 // dispatching ADD
 store.dispatch({type: "ADD"});
-// checking state
-console.log("Store is now = ", JSON.stringify(store.getState()));
 
-// // dispatching ADD
-// store.dispatch({type: "SUB"});
-// // checking state
-// console.log("Store is now = " + store.getState());
+// dispatching SUB
+store.dispatch({type: "SUB"});
 
-// // dispatching ADD
-// store.dispatch({type: "ADD"});
-// // checking state
-// console.log("Store is now = " + store.getState());
+// dispatching ADD
+store.dispatch({type: "ADD"});
+
+// dispatching SUB
+store.dispatch({type: "SUB"});
